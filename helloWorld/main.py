0bd4b139087aa0ffa4770d6e7ea80b6f9ca7cb61
orhunguven <ahmetorhun.guven@gmail.com>
2020-10-18 22:52:22 +0300

    modified to be able to use wsgi

--- helloWorld/main.py
+++ helloWorld/main.py
@@ -4,3 +4,5 @@ app = Flask(__name__)
 @app.route("/")
 def hello_www():
     return "Hello World Wide Web! I'm testing Poll SCM on Jenkikns."
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
