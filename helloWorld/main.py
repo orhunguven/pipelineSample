bc2b6a8dea8504c49f7d888745a4c3bf6c657514
orhunguven <ahmetorhun.guven@gmail.com>
2020-10-26 20:07:47 +0300

    fit for using with gunicorn wsgi

--- helloWorld/main.py
+++ helloWorld/main.py
@@ -4,5 +4,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello_www():
     return "Hello World Wide Web! I'm testing Poll SCM on Jenkikns."
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
